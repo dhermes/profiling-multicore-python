d6838e4224acdf7b8c2fa93b85dc415df8122ce3
Danny Hermes <daniel.j.hermes@gmail.com>
2017-05-23 12:03:32 -0700

    Initial commit.

---
+++ to_file_with_colors.sh
@@ -0,0 +1,3 @@
+# H/T: https://stackoverflow.com/a/39136984/1068170
+# H/T: https://stackoverflow.com/a/27399198/1068170
+script -q -c "dstat -t -c -s -C 0,1,2,3,4,5,6,7 1 12" dstat-colors.txt
---
+++ to_html_with_colors.sh
@@ -0,0 +1,2 @@
+# H/T: https://askubuntu.com/a/502456/439339
+cat dstat-colors.txt | aha --black > dstat-with-colors.html
